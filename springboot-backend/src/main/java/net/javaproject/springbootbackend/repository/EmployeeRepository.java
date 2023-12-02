c22a34dfab449c514a75c7d3d229cb96461d2e9b
abedalrhman200068 <122628610+abedalrhman200068@users.noreply.github.com>
2023-12-03 01:29:46 +0300

    upload app
    upload app

---
+++ springboot-backend/src/main/java/net/javaproject/springbootbackend/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package net.javaproject.springbootbackend.repository;
+
+import net.javaproject.springbootbackend.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
